4b1d8004cd1bf2801214884348b06574fda8eac7
Nicolas MARTIGNOLE <nicolas@touilleur-express.fr>
2010-09-11 15:01:00 +0200

    Added the AGPL license

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -1,15 +1,42 @@
+/*
+ * Copyright (c) 2010 Nicolas MARTIGNOLE
+ *
+ *     This file is part of GeekEvent.
+ *
+ *     GeekEvent is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Foobar is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *     See http://touilleur-express.fr/ for more details.
+ */
+
 import play.Play;
 import play.jobs.*;
 import play.test.*;
 
 import models.*;
 
+/**
+ * Executed when the application starts in developer mode, to load some entries
+ * into the database.
+ *
+ * @author Nicolas Martignole
+ */
 @OnApplicationStart
 public class Bootstrap extends Job {
 
     public void doJob() {
         if (Play.mode == Play.Mode.DEV) {
-            if(JugEvent.count()==0){
+            if (JugEvent.count() == 0) {
                 Fixtures.load("initial-data.yml");
             }
         }
--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2010 Nicolas MARTIGNOLE
+ *
+ *     This file is part of GeekEvent.
+ *
+ *     GeekEvent is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Foobar is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *     See http://touilleur-express.fr/ for more details.
+ */
+
 package controllers;
 
 import models.JugEvent;
@@ -6,6 +27,12 @@ import play.mvc.*;
 
 import java.util.List;
 
+
+/**
+ * Main controller, generates the home page.
+ *
+ * @author Nicolas Martignole
+ */
 public class Application extends Controller {
 
     public static void index() {
@@ -47,13 +74,13 @@ public class Application extends Controller {
     }
 
     public static void login(String email, String password) {
-        JugUser user = JugUser.authenticate(email,password);
-        if(user==null){
+        JugUser user = JugUser.authenticate(email, password);
+        if (user == null) {
             flash.error("Erreur: mot de passe non valide ou comtpe inexistant");
             index();
             return;
         }
-        Scope.Session.current().put("user",user.email);
+        Scope.Session.current().put("user", user.email);
         flash.success("Vous êtes authentifié !");
     }
 
