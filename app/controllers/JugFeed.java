d90b32fd6886032e2d81b76052f6326c9475099a
Nicolas MARTIGNOLE <nicolas@touilleur-express.fr>
2010-10-02 17:59:40 +0200

    Initial structure for JugFeed

---
+++ app/controllers/JugFeed.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2010 Nicolas MARTIGNOLE
+ *
+ * This file is part of GeekEvent.
+ *
+ * GeekEvent is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GeekEvent is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with GeekEvent.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package controllers;
+
+import play.mvc.Controller;
+
+/**
+ * Controller for the RSS feed
+ *
+ * @author Nicolas Martignole
+ * @since 2 oct. 2010 17:57:52
+ */
+public class JugFeed extends Controller {
+    public static void rss(){
+        render();
+    }
+}
